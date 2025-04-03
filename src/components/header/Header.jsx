3e6e851c04608a22354f687de8b88058ac587857
AndreaCatapano <a.catapano96@gmail.com>
2025-04-03 17:03:08 +0200

    feat: completed header with logo and nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,46 @@
 import './Header.css';
+import dcLogo from '../../assets/img/dc-logo.png'
 
 function Header() {
+
+    const navLinks = [
+        { name: 'CHARACTERS', url: '/#' },
+        { name: 'COMICS', url: '/#' },
+        { name: 'MOVIES', url: '/#' },
+        { name: 'TV', url: '/#' },
+        { name: 'GAMES', url: '/games' },
+        { name: 'COLLECTIBLES', url: '/#' },
+        { name: 'VIDEOS', url: '/#' },
+        { name: 'FANS', url: '/#' },
+        { name: 'NEWS', url: '/#' },
+        { name: 'SHOP', url: '/#' }
+    ];
+
     return (
         <header>
-            <h1>HEADER</h1>
+            <div className='header-container'>
+
+                <div className="logo-container">
+                    <a href="/">
+                        <img src={dcLogo} alt="DC Logo" className="dc-logo" />
+                    </a>
+                </div>
+
+
+                <nav className="main-nav">
+                    <ul className="nav-list">
+                        {navLinks.map((link, index) => (
+                            <li key={index} className="nav-item">
+                                <a href={link.url} className="nav-link">
+                                    {link.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+
+
+            </div>
         </header>
     );
 }
