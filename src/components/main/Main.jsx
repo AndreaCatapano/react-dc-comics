d1fc8eae3a04176b442e9bfa11016a1ee9ab73f5
AndreaCatapano <a.catapano96@gmail.com>
2025-04-07 15:44:52 +0200

    feat: added comics in main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,12 +3,20 @@ import comics from '../../data/comics.js'
 import Jumbotron from '../Jumbotron/Jumbotron.jsx';
 
 
+
+
 function Main() {
     return (
         <main>
             <Jumbotron />
             <div className='main-container'>
-                <h1> Content goes here</h1>
+
+                {comics.map((comic) => (
+                    <div key={comic.id} className="comic-card">
+                        <img src={comic.thumb} alt={comic.title} className="comic-card-image" />
+                        <h3 className="comic-card-title">{comic.series}</h3>
+                    </div>
+                ))}
             </div>
         </main>
     );
